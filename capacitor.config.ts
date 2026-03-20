8d5fa7d6f391ff0b2ebccce79f7f242761d974c4
vyshnavirakvih-cyber <vyshnavi.rakvih@gmail.com>
2026-03-20 16:14:29 +0530

    APK setup completed

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.instafitcore.official',
+  appId: 'com.instafitcore.app',
   appName: 'Instafitcore',
   webDir: 'out',
   plugins: {
